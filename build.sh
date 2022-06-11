21b9237161421d1cdeff41e2d1e89e7d9ffd09f6
llerey <llerey.yt@gmail.com>
2022-06-11 14:52:41 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -47,10 +47,10 @@ case $deviceinfo_arch in
 esac
 
 cd "$TMPDOWN"
-    [ -d llvm-r383902b ] || git clone https://github.com/kdrag0n/proton-clang --depth 1
+    [ -d proton-clang ] || git clone https://github.com/kdrag0n/proton-clang --depth 1
     GCC_PATH="$TMPDOWN/proton-clang"
     if $deviceinfo_kernel_clang_compile; then
-        [ -d clang-r383902b ] || git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b llvm-r383902b --depth 1
+        [ -d llvm-r383902b ] || git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b llvm-r383902b --depth 1
         CLANG_PATH="$TMPDOWN/linux-x86"
         rm -rf "$TMPDOWN/linux-x86/.git" "$TMPDOWN/linux-x86/"!(clang-r383902b)
     fi
